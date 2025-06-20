a04ff50f20323659cd110ac388adb87e2a3e5bb6
Lukose.Cyriac <Lukose.Cyriac@rolecserv.co.uk>
2025-06-20 14:25:17 +0100

    hosting images on uploadthing

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
     typescript: {ignoreBuildErrors: true,},
-    eslint: {ignoreDuringBuilds: true,}
+    eslint: {ignoreDuringBuilds: true,},
+    images: {remotePatterns: [ {hostname: 'dwqsg9sdff.ufs.sh'} ]}
 };
+
+
 
 export default config;
--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,12 +4,14 @@ import Link from "next/link";
 
 type FooterProps = {children: React.ReactNode;};
 
+//assets/logo/Group.svg
+
 const Footer =( ({children}: FooterProps)=> (
     <div className="flex justify-between mx-4 sm:mx-25 h-30 sm:h-80 py-5 sm:py-15 text-white">
         <div className="justify-between flex flex-col ">
             <div className=" flex flex-col space-y-3 sm:space-y-7">
                 <div className="">
-                    <Image src="/assets/logo/Group.svg" className="w-10 h-5 sm:w-20 sm:h-10" alt="logo" width={0} height={0}></Image> 
+                    <Image src="https://dwqsg9sdff.ufs.sh/f/RtbpbkCLpXS4LjOQO3BXwtF8TG9KgP1msMcpINC06AyrWUVl" className="w-10 h-5 sm:w-20 sm:h-10" alt="SVG" width={0} height={0}></Image> 
                 </div>
                 <div className="flex gap-3 sm:gap-7 ">
                     <Link href="https://www.linkedin.com/company/rolec-services/posts/?feedView=all">
@@ -25,7 +27,7 @@ const Footer =( ({children}: FooterProps)=> (
                         < Image src="/assets/social media/youtube.png" alt="Description" className="hover:opacity-90 transition-opacity object-contain w-2 h-2 sm:h-4 sm:w-4" width={0} height={0}/>
                     </Link>
                     <Link href="https://www.instagram.com/rolecserv/">
-                        < Image src="/assets/social media/insta.png" alt="Description" className="hover:opacity-90 transition-opacity object-contain w-2 h-2 sm:h-4 sm:w-4" width={0} height={0}/>
+                        < Image src="https://dwqsg9sdff.ufs.sh/f/RtbpbkCLpXS4FHr8BNa8FZkTwRCJju1asvY3KMN40iOdcnmW" alt="Description" className="hover:opacity-90 transition-opacity object-contain w-2 h-2 sm:h-4 sm:w-4" width={0} height={0}/>
                     </Link>
                 </div>
             </div>
