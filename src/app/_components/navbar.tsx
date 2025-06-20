184ca28284461bda2e185233d237efe01f44f7c0
Lukose.Cyriac <Lukose.Cyriac@rolecserv.co.uk>
2025-06-20 09:52:43 +0100

    navbar state changes

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,11 +1,23 @@
 'use client'
 import Button from "@mui/material/Button"
-import { useState } from "react";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 
 function NavBar(){
     
+    const path = usePathname();
     const [clicked, setClicked] = useState("Example");
+
+    useEffect( () => {
+        if(path.includes("/products")){
+            setClicked("Knowledge Base")
+        }else{
+            setClicked("Example");
+        }
+    },[path]);
+
+
 
     return (
         <nav className = "flex justify-between sm:justify-start items-center bg-white text-black sm:gap-20">
@@ -14,12 +26,14 @@ function NavBar(){
             </Button>
             <div className="navbar-center">
                 <div className="flex list-none m-0 p-5 sm:p-0 gap-2 sm:gap-15 text-xs sm:text-xl">
-                    <a href="/" className={`hover:font-extrabold ${clicked === "Example" ? 'font-extrabold' : ''}`} onClick={()=>setClicked("Example")}> Example </a>
                     <a href="/products" className= {`hover:font-extrabold ${clicked === "Knowledge Base" ? 'font-extrabold' : ''}`} onClick={()=>setClicked("Knowledge Base")}> Knowledge Base </a>
+                    <a href="/" className={`hover:font-extrabold ${clicked === "Example" ? 'font-extrabold' : ''}`} onClick={()=>setClicked("Example")}> Example </a>
                 </div>
             </div>
         </nav>
     )
 }
 
+
 export default NavBar
+
